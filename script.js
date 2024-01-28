892887ac5898d4f1d6a0e93bc51fa774cb4ebedf
Pauline Justine Capacia <capaciapauline@gmail.com>
2024-01-28 17:08:25 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = " ";
+}
+function calculate(){
+    display.value = eval(display.value);
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+    
+}
+
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    
+    if (key >= "0" && key <= "9") {
+        appendToDisplay(key);
+    } else if (key === "+" || key === "-" || key === "*" || key === "/") {
+        appendToDisplay(" " + key + " ");
+    } else if (key === "Enter") {
+        calculate();
+    } else if (key === "c") {
+        clearDisplay();
+    }
+});
